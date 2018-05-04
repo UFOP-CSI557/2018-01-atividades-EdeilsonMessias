d071a015167a092ee5bab073ca62d6ab8fce7ffd
Edeilson <edeilsoncm@hotmail.com>
2018-05-03 23:57:32 -0300

    Adicionar trabalho prático 1

---
+++ AGReal/src/agreal/Problema.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package agreal;
+
+/**
+ *
+ * @author fernando
+ */
+public class Problema {
+    
+    public void calcularFuncaoObjetivo(Individuo individuo) {
+        
+    	Double rastrigin = 10.0 * individuo.getVariaveis().size();
+                
+        for( Double var : individuo.getVariaveis()) {
+        	rastrigin += Math.pow(var, 2) - 10.0 * Math.cos(2 * Math.PI * var);
+        }
+        
+        individuo.setFuncaoObjetivo(rastrigin);
+        
+    }
+    
+}
